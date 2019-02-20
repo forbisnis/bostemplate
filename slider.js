caa73ef4838dbd4249ba370cc7f35622171b825d
forbisnis <47534807+forbisnis@users.noreply.github.com>
2019-02-20 19:51:02 +0700

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+//<![CDATA[
+$(document).ready(function () { if ($("#creditme,.creditme").attr("href") != "https://www.bostemplate.com") {
+        window.location.href = "https://www.bostemplate.com"; }});
+//]]></script>
